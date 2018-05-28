fabff5dc6fa99b8d614f15cd4146ae064507f167
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2018-05-28 17:13:01 +0300

    Finish

--- GuidFormat/GuidFormat/Program.cs
+++ GuidFormat/GuidFormat/Program.cs
@@ -6,7 +6,27 @@ namespace GuidFormat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] correspondingFormat = { "N", "D", "B", "P", "X" };
+
+            Console.WriteLine("Input guid's corresponding format (N, D, B, P or X) than press Enter");
+            var input = Console.ReadLine().ToUpper();
+
+            if (input != null && input.Length == 1 && Array.Exists(correspondingFormat, el => el == input))
+            {
+                Console.WriteLine($"New GUID: {GenerateGuid(input)}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid format");
+            }
+
+            Console.ReadKey();
         }
+
+        private static string GenerateGuid(string format)
+        {
+            return Guid.NewGuid().ToString(format);
+        }
+
     }
 }
